b7f9e6f16ebeb800fab9d77780e446cce24fa8b2
marco ferreira <marco@smartinnovation.com.br>
2022-02-25 14:37:46 -0300

    deploy do projeto

---
+++ clientes/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+from clientes.validators import *
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos."})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': "O NOME não deve ter dígitos."})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': "O RG deve ter 9 dígitos."})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': "O numero de celular deve seguir esse modelo: 11 91234-1234, (respeitando os espaços e traço)."})
+        return data
+
+
+    
+    
+    
